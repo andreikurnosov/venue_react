361e69d9b750afbcf26143296434cde3e5ae3800
nixdie <nixdie@yandex.ru>
2020-01-10 01:08:35 +0700

    added slider

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './resources/styles.css';
 
 import Header from './components/header/header.component';
+import Featured from './components/featured/featured.component';
 
 function App() {
   return (
     <div style={{backgroundColor:'purple', height:'1200px'}}>
       <Header />
+      <Featured />
     </div>
   );
 }
