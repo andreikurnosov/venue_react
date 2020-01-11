f2cd6ae0a5955dfc21af60ca108304ddb17308d7
nixdie <nixdie@yandex.ru>
2020-01-12 01:17:24 +0700

    Updates

---
+++ precache-manifest.0526c011b19e8c85f13f8babc50b9806.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aafff5a200064a29ba3c7bf79c860054",
+    "url": "/venue_react/index.html"
+  },
+  {
+    "revision": "162b3b039e9094be07ab",
+    "url": "/venue_react/static/css/main.e7692d97.chunk.css"
+  },
+  {
+    "revision": "5d1c0e9a90b832326a61",
+    "url": "/venue_react/static/js/2.8643411a.chunk.js"
+  },
+  {
+    "revision": "66beb09bd5c53ddc0928ab7844805f0e",
+    "url": "/venue_react/static/js/2.8643411a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "162b3b039e9094be07ab",
+    "url": "/venue_react/static/js/main.415302cd.chunk.js"
+  },
+  {
+    "revision": "ada8dce643717c846f15",
+    "url": "/venue_react/static/js/runtime-main.3974de8a.js"
+  },
+  {
+    "revision": "84753208e3ace6c36a3a29c731983a69",
+    "url": "/venue_react/static/media/slide_one.84753208.jpg"
+  },
+  {
+    "revision": "9aa09a50b7eb84e96d522c10dfaa78e5",
+    "url": "/venue_react/static/media/slide_three.9aa09a50.jpg"
+  },
+  {
+    "revision": "3a3ffd3696fea8999087bbdcf97d69d2",
+    "url": "/venue_react/static/media/slide_two.3a3ffd36.jpg"
+  }
+]);
